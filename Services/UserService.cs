ff5f39618226dd658ad7b96913f3aca366fcf6ec
Royal Benny <Royal.Benny@ust.com>
2021-12-14 08:46:34 +0530

    all services added

---
+++ Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL;
+using JeanStationModels;
+
+namespace Services
+{
+    public class UserService:IUserService
+    {
+        private readonly IUserRepository _repo;
+        public UserService(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public User CreateUser(User user)
+        {
+            try
+            {
+                return _repo.CreateUser(user);
+            }catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public User GetUser(string id)
+        {
+            try
+            {
+               return _repo.GetUser(id);
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
