4bdc45f376e52ec9c954d3a5311a95012ffaeba2
Vishnu Unnikrishnan <Vishnu.Unnikrishnan1@ust.com>
2021-12-17 12:37:26 +0530

    UserService Updated

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -2,8 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Services;
+using JeanStationModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using ServiceStack.Text;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,42 +19,90 @@ namespace UserService.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-
+        private static readonly List<User> Users = new List<User>()
+        {
+            new User(){Email="vishnu@gmail.com",Password="admin@123"},
+            new User(){Email="raj@gmail.com",Password="admin@1234"}
+        };
         private readonly IUserService _userService;
-        public UserController(IUserService userService)
+        private readonly IConfiguration _config;
+        private IConfiguration config;
+
+        public UserController(IUserService userService, IConfiguration _config)
         {
             _userService = userService;
+            _config = config;
         }
+        //IConfiguration _config;
+        //public UserController(IConfiguration config )
+        //{
+        //    _config = config;
+        //}
         // GET: api/<UserController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "User1", "User2" };
+            return Users;
         }
 
-        // GET api/<UserController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
+        
         // POST api/<UserController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("Register")]
+        public void Post([FromBody] User value)
         {
+            Users.Add(value);
+
+        }
+        [HttpPost("Validate")]
+        public string Validate([FromBody] User value)
+        {
+            if (ValidateLogin(value))
+            {
+                var token = GenerateToken(value);
+                return token.ToString();
+            }
         }
 
         // PUT api/<UserController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("{Email}")]
+        public void Put(string Email, [FromBody] User value)
         {
         }
+        private bool ValidateLogin(User u)
+        {
+            if (u == null)
+                return false;
+            User user = (User)(from us in Users
+                              where us.Email.Equals(u.Email) && us.Password==u.Password
+                              select us).FirstOrDefault();
+            if (user != null)
+                return true;
+            else
+                return false;
+        }
+        private string GenerateToken(User u)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                    new Claim(JwtRegisteredClaimNames.Email,u.Email),
+                    new Claim("Role","Admin"),
+                    new Claim("User",u.Email)
+                };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Audience"],
+              claims,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
 
-        // DELETE api/<UserController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
         }
+        //// DELETE api/<UserController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
